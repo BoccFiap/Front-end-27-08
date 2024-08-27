9e18eeb851647d80bef59ca839443224b56554e8
bocc <labsfiap@fiap.com.br>
2024-08-27 11:11:19 -0300

    turu turu

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// instalando o pacote de dependencias para criar as rotas
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'  
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
 
+/*Criando a funcao createBrowserRouter */
+const router= createBrowserRouter([
+  {
+    /* CRIANDO OS ELEMENTOS PAI */
+    path:'/', elemento:<App/>,
+    errorElement:<Error/>,
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/*criando a desestruturação para que seja renderizada */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/routes/Error.jsx
@@ -0,0 +1,8 @@
+const Error=() =>{
+    return(
+        <>
+        <h1>404-Page not foun, voce digitou errado, lerdao</h1>
+        </>
+    )
+}
+export default Error
