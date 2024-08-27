fdea4ba2778919c4597cb6b01db9c57f11ef86e7
bocc <labsfiap@fiap.com.br>
2024-08-27 11:42:21 -0300

    alguma coisa diferente

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import {link} from 'react-router-dom'
+
+const Nav=()=>{
+    return(
+        <>
+        <Link to='/'>Home </Link>
+        <Link to='/'>Aluno </Link>
+        </>
+    )
+}
+export default Nav
